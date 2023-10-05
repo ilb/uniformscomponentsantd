e6f6b7aac625dd1cbee98b3bbc22aa3654dad22e
ssortia <alexkiyan.lug@gmail.com>
2023-10-05 16:48:08 +0300

    updated select field

--- src/components/CustomSelectField/index.js
+++ src/components/CustomSelectField/index.js
@@ -1,20 +1,38 @@
 import { connectField } from 'uniforms';
 import { SelectField } from 'uniforms-antd';
 
-const CustomSelectField = ({ options, showSearch, onSearch, value }) => {
+const CustomSelectField = ({
+  options,
+  showSearch,
+  onSearch,
+  value,
+  onAfterChange,
+  onChange,
+  withEmptyOption = false,
+  ...props
+}) => {
+  if (withEmptyOption) {
+    options.unshift({
+      label: withEmptyOption?.label ?? 'Ничего не выбрано',
+      value: withEmptyOption.value ?? ''
+    });
+  }
+
   return (
-    <>
-      <SelectField
-        showSearch={showSearch}
-        options={options}
-        showInlineError
-        onSearch={(query) => onSearch && onSearch(query)}
-        filterOption={() => true}
-        value={value}
-        name=""
-      />
-    </>
+    <SelectField
+      {...props}
+      onChange={(...props) => {
+        onChange(...props);
+        onAfterChange && onAfterChange(...props);
+      }}
+      showSearch={showSearch}
+      options={options}
+      showInlineError
+      onSearch={(query) => onSearch && onSearch(query)}
+      filterOption={() => true}
+      value={value}
+      name=""
+    />
   );
 };
-
 export default connectField(CustomSelectField);
