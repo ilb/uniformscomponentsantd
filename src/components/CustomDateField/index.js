b1291451f5502488479f96057aa751f298f78c8c
ssortia <alexkiyan.lug@gmail.com>
2023-11-10 13:34:31 +0300

    revert

--- src/components/CustomDateField/index.js
+++ src/components/CustomDateField/index.js
@@ -2,32 +2,15 @@ import { connectField } from 'uniforms';
 import { DatePicker, Form } from 'antd';
 import moment from 'moment';
 import styles from './index.module.scss';
-import { useState } from 'react';
+import IMask from 'imask';
 
 const CustomDateField = connectField(
   ({ value, onChange, required, error, label, id, dateFormat = 'DD.MM.YYYY', ...props }) => {
-    const [text, setText] = useState("");
+    const masked = IMask.createMask({ mask: Date });
 
     const maskValue = (event) => {
-      let input = event.target;
-
-      if (event.key !== 'Backspace') {
-        const newText = text.concat(event.key);
-        setText(newText);
-      }
-
-      let inputValue = text.replace(/\D/g, ''); // Удаляем все символы, кроме цифр
-      let masked = inputValue;
-
-      if (inputValue.length >= 2) {
-        masked = inputValue.slice(0, 2) + '.' + inputValue.slice(2);
-      }
-
-      if (inputValue.length >= 4) {
-        masked = inputValue.slice(0, 2) + '.' + inputValue.slice(2, 4) + '.' + inputValue.slice(4);
-      }
-
-      input.value = masked;
+      masked.resolve(event.target.value);
+      event.target.value = masked.value;
     };
 
     return (
