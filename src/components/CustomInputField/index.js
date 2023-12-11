f7dc31861f774876075847fdc6a591296e9ed7eb
lianlun1 <sanzantonov49@gmail.com>
2023-12-11 10:23:42 +0400

    created pattern input field

---
+++ src/components/CustomInputField/index.js
@@ -0,0 +1,102 @@
+import classnames from 'classnames';
+import { useRef, useState } from 'react';
+import { Badge, Form } from 'antd';
+import { connectField } from 'uniforms';
+import ReactInputMask from 'react-input-mask';
+import styles from './index.module.scss';
+
+const CustomInput = ({
+  label,
+  additionalLabel,
+  value,
+  field,
+  disabled,
+  error,
+  showInlineError,
+  required,
+  validateStatus,
+  help,
+  onCheckbox,
+}) => {
+  const inputRef = useRef();
+
+  if (onCheckbox) {
+    value = '';
+  }
+
+  const inputAdditionalLabel = additionalLabel || field.uniforms?.additionalLabel;
+  const mask = field.uniforms.pattern;
+  const maskChar = field.uniforms.mask || ' ';
+  const caseMode = field.uniforms.caseMode;
+
+  const casedText = (event) => {
+    if (caseMode === 'upperCase') {
+      let target = event.target;
+      let p = target.selectionStart;
+      target.value = target.value.toUpperCase();
+      target.setSelectionRange(p, p);
+    };
+    if (caseMode === 'lowerCase') {
+      let target = event.target;
+      let p = target.selectionStart;
+      target.value = target.value.toLowerCase();
+      target.setSelectionRange(p, p);
+    }
+  };
+
+  const patternFormatProps = field.uniforms || {};
+  const inputElement = (
+    <ReactInputMask
+      mask={mask}
+      maskChar={maskChar}
+      onInput={(event) => {
+        casedText(event);
+      }}
+      {...patternFormatProps}
+    />
+  );
+  return (
+    <Form.Item className={styles.patternField} required={required} label={label}>
+      <div
+        className={classnames(
+          'ui',
+          {
+            disabled,
+            error
+          },
+          inputAdditionalLabel ? 'right labeled input' : 'input',
+          validateStatus && styles.warningInput
+        )}>
+        <div className={classnames('ant-form-item-control-input-content')}>
+          <div
+            className={classnames(
+              'ant-picker ' +
+              (error ? ' ant-picker-status-error ' : '') +
+              'ant-picker-has-feedback ' +
+              `${styles.patternPicker}`
+            )}>
+            <div className={classnames('ant-picker-input')}>
+              {inputElement}
+              {inputAdditionalLabel && <Badge>{inputAdditionalLabel}</Badge>}
+            </div>
+          </div>
+        </div>
+      </div>
+      {!!(error && showInlineError) && (
+        <div
+          className="ui red fluid basic pointing label"
+          style={{ color: '#ff4d4f', fontSize: '14px' }}>
+          {error.message}
+        </div>
+      )}
+      {help && (
+        <>
+          <p className={styles.helpText}>{help}</p>
+        </>
+      )}
+    </Form.Item>
+  );
+};
+
+const InputField = connectField(CustomInput);
+export default InputField;
